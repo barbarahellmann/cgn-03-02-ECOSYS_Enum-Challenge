482f91ce513f7fe6b718f827f3eeb68d3621ffc8
barbarahellmann <babshellmann@web.de>
2024-03-25 11:06:12 +0100

    Output for day before day type

--- org/example/Main.java
+++ org/example/Main.java
@@ -1,3 +1,13 @@
+
+//Step 1: Create an enum DaysOfWeek with the org.example.weekdays (Monday to Sunday).
+//Step 2: Write a method that returns the passed weekday as a string if it is a weekday (Monday to Friday), otherwise return 'Weekend'.
+//Step 3: Create a record 'Person' with the properties 'id', 'name', and 'favoriteDay', where 'favoriteDay' is of type DaysOfWeek.
+//Step 4: Create a class PersonRepository with a list/map of persons.
+//Step 5: Write a method in the class PersonRepository that searches for and returns a person based on their id from the list/map. The method should return an Optional.
+//Step 6: In your main method, check if the person exists. If they do, print the name and favorite day of the week to the console.
+
+
+
 package org.example;
 
 public class Main {
